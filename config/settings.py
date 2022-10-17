dea5063615c9b9fb7dc32141913cd444d5256560
Ilya Pankov <iluxaan@mail.ru>
2022-10-17 16:41:34 +0300

    feat: get bot token / write path for project dir

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+from os import getenv, path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# telegram bot token
+TELEGRAM_BOT_API_KEY = getenv('BOT_TOKEN')
+
+# project root
+BASE_DIR = path.dirname(path.abspath(__file__))
+
+# db settings
+DB_NAME = 'journal.sqlite'
+DATABASE = path.join('sqlite:///' + BASE_DIR, DB_NAME)
