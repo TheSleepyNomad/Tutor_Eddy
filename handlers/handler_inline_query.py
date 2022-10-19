4c2f68276a1ce5130d33d34d05c942f5d78004fa
Ilya Pankov <iluxaan@mail.ru>
2022-10-19 16:26:40 +0300

    refactor: prepare for refactoring

--- handlers/handler_inline_query.py
+++ handlers/handler_inline_query.py
@@ -6,14 +6,12 @@ class HandlerInlineQuery(Handler):
     def __init__(self, bot):
         super().__init__(bot)
 
-
     def test(self, call):
-        self.bot.answer_callback_query(call.id, 'Тревога', show_alert=True)
-    
+        self.bot.send_message(call.id,'хочет прийти на пробное занятие.')
+
     def handle(self):
         @self.bot.callback_query_handler(func=lambda call: True)
         def callback_inline(call):
             print(call.data)
             print(call)
             self.test(call)
-            
