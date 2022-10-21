8588a50e2190aff273d60d565f56b63e2072b02f
Ilya Pankov <iluxaan@mail.ru>
2022-10-21 10:20:30 +0300

    feat: uncomment admin role check

--- handlers/handler_commands.py
+++ handlers/handler_commands.py
@@ -23,34 +23,33 @@ class HandlerCommands(Handler):
             message.from_user.id)
 
         # check user role /admin/student/guest
-        # if check_admin_role(message.from_user.username):
-        #     self.bot.send_message(message.chat.id, 'Админ вернулся!',
-        #     reply_markup=self.keybords.admin_start_menu())
+        if check_admin_role(message.from_user.username):
+            self.bot.send_message(message.chat.id, 'Админ вернулся!',
+            reply_markup=self.keybords.admin_start_menu())
 
-        # else:
-
-        # if user already in database
-        if check_result:
-
-            # if user is student
-            if not check_result.guest_is:
-                self.bot.send_message(message.chat.id,
-                                      f'{MsgTemplates.STUDENTS_START_MSG}',
-                                      reply_markup=self.keybords.students_start_menu())
-            # if user still guest
-            else:
-                self.bot.send_message(message.chat.id,
-                                      f'{MsgTemplates.GUEST_START_MSG}',
-                                      reply_markup=self.keybords.guest_start_menu())
-        # for new visitors
         else:
-            # send welcome msg
-            self.bot.send_message(message.chat.id, f'{MsgTemplates.START_MSG}')
+            # if user already in database
+            if check_result:
 
-            # send msg for choosing language
-            self.bot.send_message(message.chat.id,
-                                  f'{MsgTemplates.CHOOSE_LANG_MSG}',
-                                  reply_markup=self.keybords.choose_language_menu())
+                # if user is student
+                if not check_result.guest_is:
+                    self.bot.send_message(message.chat.id,
+                                        f'{MsgTemplates.STUDENTS_START_MSG}',
+                                        reply_markup=self.keybords.students_start_menu())
+                # if user still guest
+                else:
+                    self.bot.send_message(message.chat.id,
+                                        f'{MsgTemplates.GUEST_START_MSG}',
+                                        reply_markup=self.keybords.guest_start_menu())
+            # for new visitors
+            else:
+                # send welcome msg
+                self.bot.send_message(message.chat.id, f'{MsgTemplates.START_MSG}')
+
+                # send msg for choosing language
+                self.bot.send_message(message.chat.id,
+                                    f'{MsgTemplates.CHOOSE_LANG_MSG}',
+                                    reply_markup=self.keybords.choose_language_menu())
 
     def pressed_help_btn(self, message: Message) -> None:
         """
